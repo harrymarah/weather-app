2922d18e177c7a0abed6cbadafd617bbcbe16dc1
Harry Marah <info@harrymarah.com>
2023-03-02 15:24:35 +0000

    Made changes for deployment on netlify as CSS did not display correctly in build.

--- src/components/info-tile.js
+++ src/components/info-tile.js
@@ -1,15 +1,15 @@
-import React from "react";
-import '../styles/info-tile.css'
+import React from 'react'
 import '../styles/glass-style.css'
-
+import '../styles/info-tile.css'
+import '../styles/pic-tile.css'
 
 const infoTile = (props) => {
-    return (
-        <div className="info-tile glass">
-            <h3 className="number-data">{props.weatherData}</h3>
-            <small className="info-text">{props.description}</small>
-        </div>
-    )
+  return (
+    <div className="pic-tile glass">
+      <h3 className="number-data">{props.weatherData}</h3>
+      <small className="info-text">{props.description}</small>
+    </div>
+  )
 }
 
 export default infoTile
--- src/components/location-display.js
+++ src/components/location-display.js
@@ -3,9 +3,11 @@ import '../styles/location-display.css'
 import '../styles/glass-style.css'
 
 const LocationDisplay = (props) => {
-    return (
-        <div className='location-heading glass'>Current Weather for {props.location}</div>
-    )
+  return (
+    <div className="location-heading glass">
+      <h2>Current Weather for {props.location}</h2>
+    </div>
+  )
 }
 
 export default LocationDisplay
